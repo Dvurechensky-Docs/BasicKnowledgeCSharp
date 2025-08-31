007785b182decfa1cfa108ef1cc5f7616f1178cc
Dvurechensky <46356631+Dvurechensky@users.noreply.github.com>
2025-08-31 07:39:36 +0300

    Auto-update headers via pre_commit.ps1 - 2025-08-31

--- LessonsAndTasks/Lesson 59 - Что такое конструктор класса, для чего, по умолчанию/Properties/AssemblyInfo.cs
+++ LessonsAndTasks/Lesson 59 - Что такое конструктор класса, для чего, по умолчанию/Properties/AssemblyInfo.cs
@@ -2,8 +2,8 @@
  * Author: Nikolay Dvurechensky
  * Site: https://sites.google.com/view/dvurechensky
  * Gmail: dvurechenskysoft@gmail.com
- * Last Updated: 30 августа 2025 08:34:30
- * Version: 1.0.11
+ * Last Updated: 31 августа 2025 07:39:22
+ * Version: 1.0.12
  */
 
 using System.Reflection;
