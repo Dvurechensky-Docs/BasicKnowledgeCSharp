31d678547b545b1657e188691b236ee4aab62f5d
Dvurechensky <46356631+Dvurechensky@users.noreply.github.com>
2025-09-09 11:11:56 +0300

    Auto-update headers via pre_commit.ps1 - 2025-09-09

--- LessonsAndTasks/Lesson 22 - Вложенные циклы. Как работают/Properties/AssemblyInfo.cs
+++ LessonsAndTasks/Lesson 22 - Вложенные циклы. Как работают/Properties/AssemblyInfo.cs
@@ -2,8 +2,8 @@
  * Author: Nikolay Dvurechensky
  * Site: https://sites.google.com/view/dvurechensky
  * Gmail: dvurechenskysoft@gmail.com
- * Last Updated: 08 сентября 2025 13:11:51
- * Version: 1.0.20
+ * Last Updated: 09 сентября 2025 11:11:42
+ * Version: 1.0.21
  */
 
 using System.Reflection;
