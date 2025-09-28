22ce9619c402ffa94d82d3eb56101080dbd4cf2b
Dvurechensky <46356631+Dvurechensky@users.noreply.github.com>
2025-09-28 21:14:10 +0300

    Auto-update headers via pre_commit.ps1 - 2025-09-28

--- LessonsAndTasks/Lesson 80 - Реализация интерфейса по умолчанию/ILogger.cs
+++ LessonsAndTasks/Lesson 80 - Реализация интерфейса по умолчанию/ILogger.cs
@@ -2,8 +2,8 @@
  * Author: Nikolay Dvurechensky
  * Site: https://sites.google.com/view/dvurechensky
  * Gmail: dvurechenskysoft@gmail.com
- * Last Updated: 27 сентября 2025 14:18:12
- * Version: 1.0.39
+ * Last Updated: 28 сентября 2025 21:13:57
+ * Version: 1.0.40
  */
 
 using System;
