57ebb3afa0a15aa1b5997143c1993d0f87e82501
Dvurechensky <46356631+Dvurechensky@users.noreply.github.com>
2025-10-18 06:51:40 +0300

    Auto-update headers via pre_commit.ps1 - 2025-10-18

--- LessonsAndTasks/Lesson 71 - НАСЛЕДОВАНИЕ/Empoloyee.cs
+++ LessonsAndTasks/Lesson 71 - НАСЛЕДОВАНИЕ/Empoloyee.cs
@@ -2,8 +2,8 @@
  * Author: Nikolay Dvurechensky
  * Site: https://sites.google.com/view/dvurechensky
  * Gmail: dvurechenskysoft@gmail.com
- * Last Updated: 17 октября 2025 13:17:46
- * Version: 1.0.59
+ * Last Updated: 18 октября 2025 06:51:28
+ * Version: 1.0.60
  */
 
 class Empoloyee : LP
