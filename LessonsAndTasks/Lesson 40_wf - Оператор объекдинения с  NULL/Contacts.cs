4d0c50a16f16e1f783e7889c7831b9fccfd33c80
Dvurechensky <46356631+Dvurechensky@users.noreply.github.com>
2025-11-08 06:51:47 +0300

    Auto-update headers via pre_commit.ps1 - 2025-11-08

--- LessonsAndTasks/Lesson 40_wf - Оператор объекдинения с  NULL/Contacts.cs
+++ LessonsAndTasks/Lesson 40_wf - Оператор объекдинения с  NULL/Contacts.cs
@@ -2,8 +2,8 @@
  * Author: Nikolay Dvurechensky
  * Site: https://sites.google.com/view/dvurechensky
  * Gmail: dvurechenskysoft@gmail.com
- * Last Updated: 07 ноября 2025 06:51:31
- * Version: 1.0.80
+ * Last Updated: 08 ноября 2025 06:51:34
+ * Version: 1.0.81
  */
 
 public class Contacts
