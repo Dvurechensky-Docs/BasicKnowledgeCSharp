7aad2dab01a8ce44d164d7582721754a6c3e8d80
Dvurechensky <46356631+Dvurechensky@users.noreply.github.com>
2025-11-16 06:51:43 +0300

    Auto-update headers via pre_commit.ps1 - 2025-11-16

--- LessonsAndTasks/Lesson 44 - Ключевое слово out, разница out и ref/Properties/AssemblyInfo.cs
+++ LessonsAndTasks/Lesson 44 - Ключевое слово out, разница out и ref/Properties/AssemblyInfo.cs
@@ -2,8 +2,8 @@
  * Author: Nikolay Dvurechensky
  * Site: https://sites.google.com/view/dvurechensky
  * Gmail: dvurechenskysoft@gmail.com
- * Last Updated: 15 ноября 2025 06:51:31
- * Version: 1.0.88
+ * Last Updated: 16 ноября 2025 06:51:31
+ * Version: 1.0.89
  */
 
 using System.Reflection;
