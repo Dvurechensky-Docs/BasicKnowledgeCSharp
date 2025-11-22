5c7d88140b5edef70b86f3e0b14673a272d14ff5
Dvurechensky <46356631+Dvurechensky@users.noreply.github.com>
2025-11-22 10:17:14 +0300

    Auto-update headers via pre_commit.ps1 - 2025-11-22

--- LessonsAndTasks/Lesson 84 - Обобщения - производительность, коллекции/SwapTestClass.cs
+++ LessonsAndTasks/Lesson 84 - Обобщения - производительность, коллекции/SwapTestClass.cs
@@ -2,8 +2,8 @@
  * Author: Nikolay Dvurechensky
  * Site: https://sites.google.com/view/dvurechensky
  * Gmail: dvurechenskysoft@gmail.com
- * Last Updated: 21 ноября 2025 09:30:35
- * Version: 1.0.94
+ * Last Updated: 22 ноября 2025 10:16:59
+ * Version: 1.0.95
  */
 
 public class SwapTestClass
