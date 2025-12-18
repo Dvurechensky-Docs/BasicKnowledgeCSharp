165cbe30b4460a8edec6f10ac944733503b8fa96
Dvurechensky <46356631+Dvurechensky@users.noreply.github.com>
2025-12-18 06:51:45 +0300

    Auto-update headers via pre_commit.ps1 - 2025-12-18

--- LessonsAndTasks/Lesson 83 - Обобщения, generic типы, методы и классы/MyList.cs
+++ LessonsAndTasks/Lesson 83 - Обобщения, generic типы, методы и классы/MyList.cs
@@ -2,8 +2,8 @@
  * Author: Nikolay Dvurechensky
  * Site: https://sites.google.com/view/dvurechensky
  * Gmail: dvurechenskysoft@gmail.com
- * Last Updated: 17 декабря 2025 15:24:47
- * Version: 1.0.120
+ * Last Updated: 18 декабря 2025 06:51:32
+ * Version: 1.0.121
  */
 
 using System;
