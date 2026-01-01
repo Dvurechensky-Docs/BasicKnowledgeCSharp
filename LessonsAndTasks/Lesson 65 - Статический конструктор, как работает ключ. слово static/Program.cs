38fff2ba6f7f6885c3cc40227326c1072d350f8a
Dvurechensky <46356631+Dvurechensky@users.noreply.github.com>
2026-01-01 16:24:05 +0300

    Auto-update headers via pre_commit.ps1 - 2026-01-01

--- LessonsAndTasks/Lesson 65 - Статический конструктор, как работает ключ. слово static/Program.cs
+++ LessonsAndTasks/Lesson 65 - Статический конструктор, как работает ключ. слово static/Program.cs
@@ -2,8 +2,8 @@
  * Author: Nikolay Dvurechensky
  * Site: https://sites.google.com/view/dvurechensky
  * Gmail: dvurechenskysoft@gmail.com
- * Last Updated: 31 декабря 2025 10:57:15
- * Version: 1.0.134
+ * Last Updated: 01 января 2026 16:23:52
+ * Version: 1.0.135
  */
 
 using System;
