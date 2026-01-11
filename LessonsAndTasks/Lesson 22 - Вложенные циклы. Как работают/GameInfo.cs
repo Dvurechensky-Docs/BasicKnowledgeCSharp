03eb708a58b904677e9e86977b0e1eb588afc862
Dvurechensky <46356631+Dvurechensky@users.noreply.github.com>
2026-01-11 17:09:06 +0300

    Auto-update headers via pre_commit.ps1 - 2026-01-11

--- LessonsAndTasks/Lesson 22 - Вложенные циклы. Как работают/GameInfo.cs
+++ LessonsAndTasks/Lesson 22 - Вложенные циклы. Как работают/GameInfo.cs
@@ -2,8 +2,8 @@
  * Author: Nikolay Dvurechensky
  * Site: https://sites.google.com/view/dvurechensky
  * Gmail: dvurechenskysoft@gmail.com
- * Last Updated: 10 января 2026 06:51:30
- * Version: 1.0.144
+ * Last Updated: 11 января 2026 17:08:54
+ * Version: 1.0.145
  */
 
 using System.Collections.Generic;
