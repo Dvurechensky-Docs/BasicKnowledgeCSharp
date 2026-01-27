4f407498e559a826fa77e43f07d038e54288dd62
Dvurechensky <46356631+Dvurechensky@users.noreply.github.com>
2026-01-27 10:40:12 +0300

    Auto-update headers via pre_commit.ps1 - 2026-01-27

--- LessonsAndTasks/Lesson 29 - Многомерные массивы. Двумерный прямоугольный массив/Program.cs
+++ LessonsAndTasks/Lesson 29 - Многомерные массивы. Двумерный прямоугольный массив/Program.cs
@@ -2,8 +2,8 @@
  * Author: Nikolay Dvurechensky
  * Site: https://sites.google.com/view/dvurechensky
  * Gmail: dvurechenskysoft@gmail.com
- * Last Updated: 26 января 2026 06:51:30
- * Version: 1.0.160
+ * Last Updated: 27 января 2026 10:39:59
+ * Version: 1.0.161
  */
 
 /*
