087663bf3817e0a7fd6134c3b1de3b444d1cd8fa
Dvurechensky <46356631+Dvurechensky@users.noreply.github.com>
2026-02-14 10:03:04 +0300

    Auto-update headers via pre_commit.ps1 - 2026-02-14

--- LessonsAndTasks/Lesson 41 - Оператор присваивания объединения со значением NULL/Program.cs
+++ LessonsAndTasks/Lesson 41 - Оператор присваивания объединения со значением NULL/Program.cs
@@ -2,8 +2,8 @@
  * Author: Nikolay Dvurechensky
  * Site: https://sites.google.com/view/dvurechensky
  * Gmail: dvurechenskysoft@gmail.com
- * Last Updated: 13 февраля 2026 11:49:53
- * Version: 1.0.178
+ * Last Updated: 14 февраля 2026 10:02:55
+ * Version: 1.0.179
  */
 
 /*
