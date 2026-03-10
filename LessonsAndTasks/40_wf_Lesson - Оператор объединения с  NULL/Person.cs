cf6a7275374def87a41c038e728c97cd2e9dd40c
Dvurechensky <46356631+Dvurechensky@users.noreply.github.com>
2026-03-10 09:14:52 +0300

    Auto-update headers via pre_commit.ps1 - 2026-03-10

--- LessonsAndTasks/40_wf_Lesson - Оператор объединения с  NULL/Person.cs
+++ LessonsAndTasks/40_wf_Lesson - Оператор объединения с  NULL/Person.cs
@@ -2,8 +2,8 @@
  * Author: Nikolay Dvurechensky
  * Site: https://sites.google.com/view/dvurechensky
  * Gmail: dvurechenskysoft@gmail.com
- * Last Updated: 09 марта 2026 06:51:41
- * Version: 1.0.202
+ * Last Updated: 10 марта 2026 09:14:37
+ * Version: 1.0.203
  */
 
 namespace Lesson_40_wf
