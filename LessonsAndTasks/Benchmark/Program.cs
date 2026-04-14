ebd7ea4e77832f9f6629c7ac9dbfb5c7c16be2cb
Dvurechensky <46356631+Dvurechensky@users.noreply.github.com>
2026-04-14 12:24:25 +0300

    Auto-update headers via pre_commit.ps1 - 2026-04-14

--- LessonsAndTasks/Benchmark/Program.cs
+++ LessonsAndTasks/Benchmark/Program.cs
@@ -2,8 +2,8 @@
  * Author: Nikolay Dvurechensky
  * Site: https://dvurechensky.pro/
  * Gmail: dvurechenskysoft@gmail.com
- * Last Updated: 13 апреля 2026 12:58:47
- * Version: 1.0.237
+ * Last Updated: 14 апреля 2026 12:24:11
+ * Version: 1.0.238
  */
 
 using BenchmarkDotNet.Attributes;
