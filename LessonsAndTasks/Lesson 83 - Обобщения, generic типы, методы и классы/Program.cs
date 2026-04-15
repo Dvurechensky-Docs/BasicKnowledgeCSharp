25a1e6b102c7a52a35b58913d1936af7576a156e
Dvurechensky <46356631+Dvurechensky@users.noreply.github.com>
2026-04-15 06:51:26 +0300

    Auto-update headers via pre_commit.ps1 - 2026-04-15

--- LessonsAndTasks/Lesson 83 - Обобщения, generic типы, методы и классы/Program.cs
+++ LessonsAndTasks/Lesson 83 - Обобщения, generic типы, методы и классы/Program.cs
@@ -2,8 +2,8 @@
  * Author: Nikolay Dvurechensky
  * Site: https://dvurechensky.pro/
  * Gmail: dvurechenskysoft@gmail.com
- * Last Updated: 14 апреля 2026 12:24:11
- * Version: 1.0.238
+ * Last Updated: 15 апреля 2026 06:51:12
+ * Version: 1.0.239
  */
 
 using System;
