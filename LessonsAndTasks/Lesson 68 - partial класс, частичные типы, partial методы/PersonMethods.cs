5bf28235bf1d9b26921c1468bc011f6e78bd0e4c
Dvurechensky <46356631+Dvurechensky@users.noreply.github.com>
2026-04-24 06:51:22 +0300

    Auto-update headers via pre_commit.ps1 - 2026-04-24

--- LessonsAndTasks/Lesson 68 - partial класс, частичные типы, partial методы/PersonMethods.cs
+++ LessonsAndTasks/Lesson 68 - partial класс, частичные типы, partial методы/PersonMethods.cs
@@ -2,8 +2,8 @@
  * Author: Nikolay Dvurechensky
  * Site: https://dvurechensky.pro/
  * Gmail: dvurechenskysoft@gmail.com
- * Last Updated: 23 апреля 2026 06:50:59
- * Version: 1.0.247
+ * Last Updated: 24 апреля 2026 06:51:09
+ * Version: 1.0.248
  */
 
 partial class Person
