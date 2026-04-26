93eab5528571aa8b73c20531496c9a08c9b955d0
Dvurechensky <46356631+Dvurechensky@users.noreply.github.com>
2026-04-26 06:52:43 +0300

    Auto-update headers via pre_commit.ps1 - 2026-04-26

--- LessonsAndTasks/Lesson 40_wf - Оператор объекдинения с  NULL/Properties/Settings.Designer.cs
+++ LessonsAndTasks/Lesson 40_wf - Оператор объекдинения с  NULL/Properties/Settings.Designer.cs
@@ -2,8 +2,8 @@
  * Author: Nikolay Dvurechensky
  * Site: https://dvurechensky.pro/
  * Gmail: dvurechenskysoft@gmail.com
- * Last Updated: 25 апреля 2026 08:10:01
- * Version: 1.0.249
+ * Last Updated: 26 апреля 2026 06:52:29
+ * Version: 1.0.250
  */
 
 //------------------------------------------------------------------------------
