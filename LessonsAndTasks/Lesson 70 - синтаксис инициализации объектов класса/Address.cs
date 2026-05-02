ce6fd38d329dc8c0e2b25b4d904be32df8897bc7
Dvurechensky <46356631+Dvurechensky@users.noreply.github.com>
2026-05-02 19:15:53 +0300

    Auto-update headers via pre_commit.ps1 - 2026-05-02

--- LessonsAndTasks/Lesson 70 - синтаксис инициализации объектов класса/Address.cs
+++ LessonsAndTasks/Lesson 70 - синтаксис инициализации объектов класса/Address.cs
@@ -2,8 +2,8 @@
  * Author: Nikolay Dvurechensky
  * Site: https://dvurechensky.pro/
  * Gmail: dvurechenskysoft@gmail.com
- * Last Updated: 01 мая 2026 06:51:17
- * Version: 1.0.256
+ * Last Updated: 02 мая 2026 19:15:39
+ * Version: 1.0.258
  */
 
 class Address
