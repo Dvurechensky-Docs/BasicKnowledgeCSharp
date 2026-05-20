ba6725bde9859e7ec689a45fd4b1139518ae606c
Dvurechensky <46356631+Dvurechensky@users.noreply.github.com>
2026-05-20 10:03:03 +0300

    Auto-update headers via pre_commit.ps1 - 2026-05-20

--- LessonsAndTasks/Lesson 73 - Операторы as is, наследование и приведение типов/Program.cs
+++ LessonsAndTasks/Lesson 73 - Операторы as is, наследование и приведение типов/Program.cs
@@ -2,8 +2,8 @@
  * Author: Nikolay Dvurechensky
  * Site: https://dvurechensky.pro/
  * Gmail: dvurechenskysoft@gmail.com
- * Last Updated: 19 мая 2026 10:14:29
- * Version: 1.0.275
+ * Last Updated: 20 мая 2026 10:02:49
+ * Version: 1.0.276
  */
 
 using System;
