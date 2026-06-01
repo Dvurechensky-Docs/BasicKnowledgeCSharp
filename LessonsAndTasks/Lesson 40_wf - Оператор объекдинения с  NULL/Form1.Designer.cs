6c48c7e3d475966c3c86c7caec255dd293d172bf
Dvurechensky <46356631+Dvurechensky@users.noreply.github.com>
2026-06-01 14:46:13 +0300

    Auto-update headers via pre_commit.ps1 - 2026-06-01

--- LessonsAndTasks/Lesson 40_wf - Оператор объекдинения с  NULL/Form1.Designer.cs
+++ LessonsAndTasks/Lesson 40_wf - Оператор объекдинения с  NULL/Form1.Designer.cs
@@ -2,8 +2,8 @@
  * Author: Nikolay Dvurechensky
  * Site: https://dvurechensky.pro/
  * Gmail: dvurechenskysoft@gmail.com
- * Last Updated: 31 мая 2026 14:54:13
- * Version: 1.0.287
+ * Last Updated: 01 июня 2026 14:45:58
+ * Version: 1.0.288
  */
 
 partial class Form1
