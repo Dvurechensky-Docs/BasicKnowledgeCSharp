6da08f4407b80e8c42dc3cc97124eaec399d5c41
Dvurechensky <46356631+Dvurechensky@users.noreply.github.com>
2026-06-24 10:33:42 +0300

    Auto-update headers via pre_commit.ps1 - 2026-06-24

--- LessonsAndTasks/Lesson 70 - синтаксис инициализации объектов класса/Person.cs
+++ LessonsAndTasks/Lesson 70 - синтаксис инициализации объектов класса/Person.cs
@@ -2,8 +2,8 @@
  * Author: Nikolay Dvurechensky
  * Site: https://dvurechensky.pro/
  * Gmail: dvurechenskysoft@gmail.com
- * Last Updated: 23 июня 2026 15:53:39
- * Version: 1.0.310
+ * Last Updated: 24 июня 2026 10:33:25
+ * Version: 1.0.311
  */
 
 class Person
