9d7dcc3eb4ad3cdf17e5bca3f5e3dd2f894933dd
Dvurechensky <46356631+Dvurechensky@users.noreply.github.com>
2026-06-26 06:51:29 +0300

    Auto-update headers via pre_commit.ps1 - 2026-06-26

--- LessonsAndTasks/Lesson 43 - Ключевое слово ref, что это и предназначение/Program.cs
+++ LessonsAndTasks/Lesson 43 - Ключевое слово ref, что это и предназначение/Program.cs
@@ -2,8 +2,8 @@
  * Author: Nikolay Dvurechensky
  * Site: https://dvurechensky.pro/
  * Gmail: dvurechenskysoft@gmail.com
- * Last Updated: 25 июня 2026 16:29:26
- * Version: 1.0.312
+ * Last Updated: 26 июня 2026 06:51:13
+ * Version: 1.0.313
  */
 
 using System;
