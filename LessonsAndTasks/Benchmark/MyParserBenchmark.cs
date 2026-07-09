11794b00cafddcce7845422a663500935510abe6
Dvurechensky <46356631+Dvurechensky@users.noreply.github.com>
2026-07-09 07:00:38 +0300

    Auto-update headers via pre_commit.ps1 - 2026-07-09

--- LessonsAndTasks/Benchmark/MyParserBenchmark.cs
+++ LessonsAndTasks/Benchmark/MyParserBenchmark.cs
@@ -2,8 +2,8 @@
  * Author: Nikolay Dvurechensky
  * Site: https://dvurechensky.pro/
  * Gmail: dvurechenskysoft@gmail.com
- * Last Updated: 08 июля 2026 07:25:54
- * Version: 1.0.326
+ * Last Updated: 09 июля 2026 07:00:20
+ * Version: 1.0.327
  */
 
 using BenchmarkDotNet.Attributes;
