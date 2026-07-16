f4c4a48f4ce74a529341fdd8ad63da361031d3d9
Dvurechensky <46356631+Dvurechensky@users.noreply.github.com>
2026-07-16 14:12:49 +0300

    Auto-update headers via pre_commit.ps1 - 2026-07-16

--- LessonsAndTasks/Task Home 2 - Введите три числа и выведите их произведение и сумму/Program.cs
+++ LessonsAndTasks/Task Home 2 - Введите три числа и выведите их произведение и сумму/Program.cs
@@ -2,8 +2,8 @@
  * Author: Nikolay Dvurechensky
  * Site: https://dvurechensky.pro/
  * Gmail: dvurechenskysoft@gmail.com
- * Last Updated: 15 июля 2026 11:52:47
- * Version: 1.0.333
+ * Last Updated: 16 июля 2026 14:12:32
+ * Version: 1.0.334
  */
 
 using System;
