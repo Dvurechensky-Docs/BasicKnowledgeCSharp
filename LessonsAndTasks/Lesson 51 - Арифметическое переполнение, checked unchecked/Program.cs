19030d4fca68daaacb384b6fad964394ffa24cf5
Dvurechensky <46356631+Dvurechensky@users.noreply.github.com>
2026-09-19 08:44:50 +0300

    Auto-update headers via pre_commit.ps1 - 2026-09-19

--- LessonsAndTasks/Lesson 51 - Арифметическое переполнение, checked unchecked/Program.cs
+++ LessonsAndTasks/Lesson 51 - Арифметическое переполнение, checked unchecked/Program.cs
@@ -2,8 +2,8 @@
  * Author: Nikolay Dvurechensky
  * Site: https://dvurechensky.pro/
  * Gmail: dvurechenskysoft@gmail.com
- * Last Updated: 18 сентября 2026 08:12:39
- * Version: 1.0.398
+ * Last Updated: 19 сентября 2026 08:44:34
+ * Version: 1.0.399
  */
 
 using System;
